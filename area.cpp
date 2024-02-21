58084d29e773bb23819b7314fd7616361c8c65c8
AMANKUMAR-Manipal <amankumarvmc10@gmail.com>
2024-02-22 02:34:17 +0530

    area calulator for a circle

---
+++ area.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+float radius,pi,area;
+cout<<"enter the radius:";
+cin>>radius;
+pi=3.1415;
+area = pi*radius*radius;
+cout<<"the area is:"<<area;
+}
